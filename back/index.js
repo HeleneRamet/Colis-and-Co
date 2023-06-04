4a7b87aecf9a5418f0ee3cf04c350463435853e1
HeleneRamet <helene.ramet@gmail.com>
2023-06-04 15:23:17 +0200

    CORS vide

--- back/index.js
+++ back/index.js
@@ -12,11 +12,11 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-const corsConfig = {
+/* const corsConfig = {
   origin: ['https://projet-colis-and-co.vercel.app/', 'https://colis-and-co.up.railway.app/'],
-};
+}; */
 
-app.use(cors(corsConfig));
+app.use(cors());
 
 // Configure body-parser middleware
 app.use(bodyParser.json());
